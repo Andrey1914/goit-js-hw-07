28b3bb6ec581c774fdf8a905931640d4724e963e
Andrey1914 <anlikajw@gmail.com>
2022-05-24 23:05:28 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,53 +1,69 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector(".gallery");
-// const output = document.querySelector(".output");
-
-// gallery.addEventListener("click", selectColor);
-
-// // This is where delegation «magic» happens
-// function selectColor(event) {
-//   if (event.target.nodeName !== "BUTTON") {
-//     return;
-//   }
-
-//   const selectedColor = event.target.dataset.color;
-//   output.textContent = `Selected color: ${selectedColor}`;
-//   output.style.color = selectedColor;
-// }
-
-// // Some helper functions to render palette items
-// createPaletteItems();
-
-// function createPaletteItems() {
-//   const items = [];
-//   for (let i = 0; i < 60; i++) {
-//     const color = getRangomColor();
-//     const item = document.createElement("button");
-//     item.type = "button";
-//     item.dataset.color = color;
-//     item.style.backgroundColor = color;
-//     item.classList.add("item");
-//     items.push(item);
-//   }
-//   colorPalette.append(...items);
-// }
-
-// function getRangomColor() {
-//   return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
-// }
-
-// function getRandomHex() {
-//   return Math.round(Math.random() * 256)
-//     .toString(16)
-//     .padStart(2, "0");
-// }
-console.log(galleryItems);
-
-import * as basicLightbox from "basiclightbox";
 
 const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
+    <img src=${original} width="800" height="600">
 `);
 
 instance.show();
+console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+  )
+  .join("");
+gallery.innerHTML(galleryMarkup);
+// const galleryMarkup = galleryItems.reduce(
+//   (acc, galleryItem) => acc + createGalleryItem(galleryItem),
+//   ""
+// );
+// gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+// instance.classList.add("gallery__image");
+
+// refs.gallery.addEventListener("click", onGalleryClick);
+// refs.btn.addEventListener("click", onClickHandlerClose);
+// refs.modal.addEventListener("click", closeLightbox);
+
+// function onGalleryClick(e) {
+//   e.preventDefault();
+//   if (e.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   if (e.target.nodeName === "IMG") {
+//     refs.lightbox.classList.add("is-open");
+//     refs.lightbox__image.src = e.target.getAttribute("data-source");
+//     refs.lightbox__image.alt = e.target.alt;
+//   }
+//   window.addEventListener("keyup", clickKey);
+// }
+
+// function onClickHandlerClose(e) {
+//   e.preventDefault();
+//   refs.lightbox.classList.remove("is-open");
+//   refs.lightbox__image.src = "";
+//   refs.lightbox__image.alt = "";
+//   window.removeEventListener("keyup", clickKey);
+// }
+
+function closeLightbox(event) {
+  if (event.target === event.currentTarget) {
+    onClickHandlerClose();
+  }
+}
+
+function clickKey(event) {
+  if (event.code === "Escape") {
+    onClickHandlerClose();
+  }
+}
