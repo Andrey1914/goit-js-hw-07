d3c088f1cb874c10851e89d5ad95ff5be9af97a9
Andrey1914 <anlikajw@gmail.com>
2022-05-28 00:05:51 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const instance = basicLightbox.create(`
-    <img src="./gallery-items.js" width="800" height="600">
-`);
-
 console.log(galleryItems);
 const gallery = document.querySelector(".gallery");
 const galleryMarkup = galleryItems
@@ -24,65 +20,26 @@ const galleryMarkup = galleryItems
   .join("");
 gallery.innerHTML = galleryMarkup;
 
-galleryItems.element.addEventListener("click", () => {
-  instance.show();
-  // instance.style.display = "block";
-  // gallery.src = this.dataset.source;
-});
-gallery.element.addEventListener("click", onGalleryClick);
+gallery.addEventListener("click", onGalleryClick);
+
 function onGalleryClick(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
+
+  if (event.target.nodeName === "IMG") {
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+    instance.show();
+  }
 }
-// gallery.forEach((element) => {
-//   element.addEventListener("click", function () {
-//     instance.style.display = "block";
-//     gallery.src = this.dataset.source;
-//   });
-// });
-// instance.show();
-// gallery.element.addEventListener("click", onGalleryClick);
-// function onGalleryClick(event) {
-//   event.preventDefault();
-//   if (event.target.nodeName !== "IMG") {
-//     return;
-//   }
-// }
-// instance.close(() => {
-//   instance.style.display = "none";
-// });
-// gallery.element.addEventListener("click", () => {
-//   instance.close();
-//   // instance.style.display = "none";
-// });
 
-// gallery.addEventListener("click", onGalleryClick);
-// refs.btn.addEventListener("click", onClickHandlerClose);
-// refs.modal.addEventListener("click", closeLightbox);
+gallery.addEventListener("click", clickKey);
 
-// function onGalleryClick(event) {
-//   event.preventDefault();
-//   if (event.target.nodeName !== "IMG") {
-//     return;
-//   }
-//   if (event.target.nodeName === "IMG") {
-//     basicLightbox.classList.add("is-open");
-//     gallery__image.src = event.target.getAttribute("data-source");
-//     gallery__image.alt = event.target.alt;
-//   }
-//   window.addEventListener("keyup", clickKey);
-// }
-
-// function closeLightbox(event) {
-//   if (event.target === event.currentTarget) {
-//     onClickHandlerClose();
-//   }
-// }
-
-// function clickKey(event) {
-//   if (event.code === "Escape") {
-//     onClickHandlerClose();
-//   }
-// }
+function clickKey(event) {
+  if (event.code === "Escape") {
+    const instance = instance.close();
+  }
+}
